77614840291c63361f5463c638eae271f2035223
Mehedi <mehedi@augnitive.com>
2022-12-12 07:56:50 +0600

    changes made for test purpuse

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -10,7 +10,7 @@ class ProductController extends Controller
 
     public function get_popular_products(Request $request){
 
-        $list = Food::where('type_id', 2)->take(10)->get();
+        $list = Food::where('type_id', 2)->take(10)->orderBy('created_at', 'DESC')->get();
 
                 foreach ($list as $item){
                     $item['description']=strip_tags($item['description']);
@@ -30,7 +30,7 @@ class ProductController extends Controller
 
     }
         public function get_recommended_products(Request $request){
-        $list = Food::where('type_id', 3)->take(10)->get();
+        $list = Food::where('type_id', 3)->take(10)->orderBy('created_at', 'DESC')->get();
 
                 foreach ($list as $item){
                     $item['description']=strip_tags($item['description']);
@@ -70,7 +70,7 @@ class ProductController extends Controller
     }
 
     public function get_drinks(Request $request){
-        $list = Food::where('type_id', 4)->take(10)->get();
+        $list = Food::where('type_id', 4)->take(10)->orderBy('created_at', 'DESC')->get();
 
                 foreach ($list as $item){
                     $item['description']=strip_tags($item['description']);
